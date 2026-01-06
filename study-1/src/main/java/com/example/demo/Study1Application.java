900c1db8f72572281a53500f0ac6dc5c691f38e9
SteveJeon95 <ttottiyaho@gmail.com>
2026-01-06 15:02:29 +0900

    git 최초 커밋

---
+++ study-1/src/main/java/com/example/demo/Study1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study1Application {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(Study1Application.class, args);
+	}
+
+}
