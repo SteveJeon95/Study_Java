b2b85b0adbfdc1b989a9244ed6b521a220bdff8e
SteveJeon95 <ttottiyaho@gmail.com>
2026-01-09 16:18:17 +0900

    최초 반영

--- study-1/src/main/java/com/example/demo/Study1Application.java
+++ study-1/src/main/java/com/example/demo/Study1Application.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Study1Application {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
 		SpringApplication.run(Study1Application.class, args);
 	}
 
